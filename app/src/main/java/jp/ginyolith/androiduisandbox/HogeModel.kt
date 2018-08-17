d854b0e72a505acad3c5e482a95af2b774679bd5
Fumiya Akagi <fumiya.akagi@iridge.jp>
2018-08-17 18:36:36 +0900

    implemented

--- app/src/main/java/jp/ginyolith/androiduisandbox/HogeModel.kt
+++ app/src/main/java/jp/ginyolith/androiduisandbox/HogeModel.kt
@@ -1,2 +1,6 @@
 package jp.ginyolith.androiduisandbox
 
+data class HogeModel(val text : String) {
+    var isTop : Boolean = false
+    var isBottom : Boolean = false
+}
--- app/src/main/java/jp/ginyolith/androiduisandbox/MainActivity.kt
+++ app/src/main/java/jp/ginyolith/androiduisandbox/MainActivity.kt
@@ -1,12 +1,45 @@
 package jp.ginyolith.androiduisandbox
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import jp.ginyolith.androiduisandbox.databinding.ActivityMainBinding
+import jp.ginyolith.androiduisandbox.databinding.ItemListBinding
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.list.adapter = ArticleListAdapter(listOf("hoge","fuga","foo", "bar").map { HogeModel(it) })
+        binding.list.layoutManager = LinearLayoutManager(this)
     }
+
+    class ArticleListAdapter(private val dataList : List<HogeModel>)
+        : RecyclerView.Adapter<ArticleListAdapter.BindingHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BindingHolder {
+            val inflater : LayoutInflater = LayoutInflater.from(parent.context)
+            val binding = ItemListBinding.inflate(inflater, parent, false)
+            return BindingHolder(binding)
+        }
+
+        override fun getItemCount(): Int = dataList.size
+
+        override fun onBindViewHolder(holder: BindingHolder, position: Int) {
+            val model= dataList[position]
+            model.isTop = position == 0
+            model.isBottom = position == dataList.size - 1
+
+            holder.binding.model = model
+        }
+
+        class BindingHolder(var binding : ItemListBinding) : RecyclerView.ViewHolder(binding.root)
+    }
+
 }

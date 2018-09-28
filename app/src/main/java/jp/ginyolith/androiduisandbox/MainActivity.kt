98e47924900eef85623f072250a5f5ef4bc60f3f
Fumiya Akagi <fumiya.akagi@iridge.jp>
2018-09-28 17:49:24 +0900

    implement

--- app/src/main/java/jp/ginyolith/androiduisandbox/MainActivity.kt
+++ app/src/main/java/jp/ginyolith/androiduisandbox/MainActivity.kt
@@ -1,12 +1,17 @@
 package jp.ginyolith.androiduisandbox
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import com.airbnb.lottie.LottieAnimationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lottie = findViewById<LottieAnimationView>(R.id.lottie)
+
     }
 }
